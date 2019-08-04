5af36aefff777c909427c50b44d2950c5906be12
Chris <Sigynliri@gmail.com>
2019-08-04 23:09:11 +0700

    этой хуй пойми какой по счету коммит

---
+++ file.js
@@ -0,0 +1,15 @@
+var films = {
+title: "Star Wars",
+genres: "Horror",
+runtime: 109, 
+};
+console.log(films.genres);
+
+let genres = 'si-fi';
+console.log(genres);
+
+
+var text = 'You\'re right, it can\'t be a quote';
+console.log(text);
+var grittings = prompt('Wath\'s your name?');
+alert('greetings and salutations');
